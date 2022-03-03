8b29794ccf9e9c164b7c6431f497e53b880fe1f8
GaV1S <gav1s@yandex.ru>
2022-03-03 15:58:22 +0300

    homework2 finished

--- app/src/main/java/ru/gav1s/picoftheday2/ui/MainActivity.kt
+++ app/src/main/java/ru/gav1s/picoftheday2/ui/MainActivity.kt
@@ -1,15 +1,25 @@
 package ru.gav1s.picoftheday2.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import ru.gav1s.picoftheday2.R
 import ru.gav1s.picoftheday2.ui.main.MainFragment
 
+
 class MainActivity : AppCompatActivity() {
+    private var sharedPreferences: SharedPreferences? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedPreferences = getSharedPreferences(App.APP_PREF_NAME, Context.MODE_PRIVATE)
+        sharedPreferences?.let {
+            val themeId = it.getInt(App.PREF_THEME_KEY, R.style.AppThemeLight)
+            setTheme(themeId)
+        }
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
+        savedInstanceState ?: let {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
